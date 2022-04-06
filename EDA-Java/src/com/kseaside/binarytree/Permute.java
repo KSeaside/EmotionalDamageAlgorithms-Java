4d71d254370bf865e1af4270e46cae75047b5c22
635722800@qq.com <635722800@qq.com>
2022-04-07 00:04:34 +0800

    全排列Test

--- EDA-Java/src/com/kseaside/binarytree/Permute.java
+++ EDA-Java/src/com/kseaside/binarytree/Permute.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class Permute {
     List<List<Integer>> res = new LinkedList<>();
 
+
     /* 主函数，输入一组不重复的数字，返回它们的全排列 */
     List<List<Integer>> permute(int[] nums) {
         // 记录「路径」
