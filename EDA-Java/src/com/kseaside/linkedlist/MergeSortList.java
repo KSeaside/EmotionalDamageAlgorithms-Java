de2831435f57f507d89a5180438ca99e1ee48cba
635722800@qq.com <635722800@qq.com>
2022-04-11 23:19:28 +0800

    合并有序链表

---
+++ EDA-Java/src/com/kseaside/linkedlist/MergeSortList.java
@@ -0,0 +1,19 @@
+package com.kseaside.linkedlist;
+// 合并有序链表
+public class MergeSortList {
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        } else if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+
+    }
+}
