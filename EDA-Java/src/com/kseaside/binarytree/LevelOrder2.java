2595749696dc685fe705198e89183d0dd9398e19
635722800@qq.com <635722800@qq.com>
2022-04-28 00:35:44 +0800

    之字打印二叉树

---
+++ EDA-Java/src/com/kseaside/binarytree/LevelOrder2.java
@@ -0,0 +1,34 @@
+package com.kseaside.binarytree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 剑指 Offer 32 - III. 从上到下打印二叉树 III
+ *
+ * 请实现一个函数按照之字形顺序打印二叉树，即第一行按照从左到右的顺序打印，第二层按照从右到左的顺序打印，
+ * 第三行再按照从左到右的顺序打印，其他行以此类推。
+ */
+public class LevelOrder2 {
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        if(root != null) queue.add(root);
+        while(!queue.isEmpty()) {
+            LinkedList<Integer> tmp = new LinkedList<>();
+            for(int i = queue.size(); i > 0; i--) {
+                TreeNode node = queue.poll();
+                if(res.size() % 2 == 0) tmp.addLast(node.val); // 偶数层 -> 队列头部
+                else tmp.addFirst(node.val); // 奇数层 -> 队列尾部
+                if(node.left != null) queue.add(node.left);
+                if(node.right != null) queue.add(node.right);
+            }
+            res.add(tmp);
+        }
+        return res;
+    }
+
+}
