297920b9eed7095ede4ad5c3c1ed2af7b68fb175
klf <konglingfan@sto.cn>
2024-07-25 14:52:45 +0800

    feat: 14

---
+++ EDA-Java/src/com/kseaside/hot100/LC14.java
@@ -0,0 +1,33 @@
+package com.kseaside.hot100;
+
+/**
+ * @Description: https://leetcode.cn/problems/longest-common-prefix/description/?envType=study-plan-v2&envId=top-interview-150
+ * @author: kray
+ * @date: 2024/7/25 14:01
+ **/
+public class LC14 {
+    /**
+     * 编写一个函数来查找字符串数组中的最长公共前缀。
+     *
+     * 如果不存在公共前缀，返回空字符串 ""。
+     * @param strs
+     * @return
+     */
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        int length = strs[0].length();
+        int count = strs.length;
+        for (int i = 0; i < length; i++) {
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < count; j++) {
+                if (i == strs[j].length() || strs[j].charAt(i) != c) {
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+        return strs[0];
+    }
+
+}
